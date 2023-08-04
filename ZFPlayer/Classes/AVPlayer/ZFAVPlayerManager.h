3f93c0ec4ce4886b6b01535bb6c8ef2b8a551fb7
RuanR <1525196995@qq.com>
2023-08-04 18:53:23 +0800

    Update ZFAVPlayerManager.h
    @property (nonatomic, strong, readonly) AVPlayerItemVideoOutput *output;

--- ZFPlayer/Classes/AVPlayer/ZFAVPlayerManager.h
+++ ZFPlayer/Classes/AVPlayer/ZFAVPlayerManager.h
@@ -35,6 +35,7 @@
 @property (nonatomic, strong, readonly) AVURLAsset *asset;
 @property (nonatomic, strong, readonly) AVPlayerItem *playerItem;
 @property (nonatomic, strong, readonly) AVPlayer *player;
+@property (nonatomic, strong, readonly) AVPlayerItemVideoOutput *output;
 @property (nonatomic, assign) NSTimeInterval timeRefreshInterval;
 /// 视频请求头
 @property (nonatomic, strong) NSDictionary *requestHeader;
